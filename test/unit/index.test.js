956f6fa2ba5779d2a5e979f2c82e64e0056df2ad
Lewis Daly <lewisdaly@me.com>
2019-11-06 15:19:08 +0800

    Working on new tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -26,49 +26,59 @@ Test('logger', function (loggerTest) {
     t.end()
   })
 
-  loggerTest.test('configure Winston', function (assert) {
-    assert.ok(Winston.transports.Console, Sinon.match({ timestamp: true, colorize: true }))
-    assert.end()
-  })
-
-  loggerTest.test('log debug level', function (assert) {
-    Logger.debug('test %s', 'me')
-    assert.ok(Sinon.match('debug', 'test me'))
-    assert.end()
-  })
-
-  // loggerTest.test('log error level, when filtered out', function (assert) {
-  //   const env = process.env
-  //   process.env.LOG_FILTER = 'info, debug'
-  //   const LoggerProxy = Proxyquire('../../src/index', {})
-  //   console.log('LoggerProxy', LoggerProxy)
-  //   LoggerProxy.error('test %s', 'me')
-  //   assert.ok(Sinon.match('error', 'test me'))
-  //   process.env = env
+  // loggerTest.test('configure Winston', function (assert) {
+  //   assert.ok(Winston.transports.Console, Sinon.match({ timestamp: true, colorize: true }))
   //   assert.end()
   // })
 
-  loggerTest.test('log info level', function (assert) {
-    const infoMessage = 'things are happening'
-    Logger.info(infoMessage)
-    assert.ok(Sinon.match('info', infoMessage))
+  // loggerTest.test('log debug level', function (assert) {
+  //   Logger.debug('test %s', 'me')
+  //   assert.ok(Sinon.match('debug', 'test me'))
+  //   assert.end()
+  // })
+
+  loggerTest.test('log error level, when filtered out', function (assert) {
+    // Arrange
+    process.env.LOG_FILTER = 'info, debug'
+    const LoggerProxy = Proxyquire('../../src/index', {})
+    console.log('LoggerProxy', LoggerProxy)
+    
+    // Act
+    LoggerProxy.error('test %s', 'me')
+    
+    // Assert
+    assert.ok(Sinon.match('error', 'test me'))
+
+    delete process.env.LOG_FILTER
     assert.end()
   })
 
-  loggerTest.test('log warn level', function (assert) {
-    const warnMessage = 'something bad is happening'
-    Logger.warn(warnMessage)
-    assert.ok(Sinon.match('warn', warnMessage))
-    assert.end()
-  })
+  // loggerTest.test('log info level', function (assert) {
+  //   const infoMessage = 'things are happening'
+  //   Logger.info(infoMessage)
+  //   assert.ok(Sinon.match('info', infoMessage))
+  //   assert.end()
+  // })
 
-  loggerTest.test('log error level', function (assert) {
-    const errorMessage = 'there was an exception'
-    const ex = new Error()
-    Logger.error(errorMessage, ex)
-    assert.ok(Sinon.match('error', errorMessage))
-    assert.end()
-  })
+  // loggerTest.test('log warn level', function (assert) {
+  //   const warnMessage = 'something bad is happening'
+  //   Logger.warn(warnMessage)
+  //   assert.ok(Sinon.match('warn', warnMessage))
+  //   assert.end()
+  // })
+
+  // loggerTest.test('log error level', function (assert) {
+  //   const errorMessage = 'there was an exception'
+  //   const ex = new Error()
+  //   Logger.error(errorMessage, ex)
+  //   assert.ok(Sinon.match('error', errorMessage))
+  //   assert.end()
+  // })
+
+  //Tests TODO:
+  //Fails to init when LOG_TRANSPORT=file, but filename isn't set
+  //logs to console when LOG_TRANSPORT=console
+  //logs to file when LOG_TRANSPORT=file
 
   loggerTest.end()
 })
--- test/unit/lib/config.test.js
+++ test/unit/lib/config.test.js
@@ -1,6 +1,48 @@
+'use strict'
+
+const Test = require('tapes')(require('tape'))
+const Sinon = require('sinon')
+const Winston = require('winston')
+const Proxyquire = require('proxyquire')
+
+Test('config', (configTest) => {
+  let sandbox
+
+  configTest.afterEach(t => {
+    delete process.env.LOG_LEVEL
+    delete process.env.LOG_FILTER
+
+    t.end()
+  })
+
+  configTest.test('process.env.LOG_LEVEL overrides the default.json value', assert => {
+    // Arrange
+    process.env.LOG_LEVEL = 'random_level'
+
+    // Act
+    const config = Proxyquire('../../../src/lib/config', {})
+    
+    // Assert
+    assert.equal(config.level, 'random_level', 'Log levels match')
+    assert.end()
+  })
+
+  configTest.test('process.env.LOG_FILTER overrides the default.json value', assert => {
+    // Arrange
+    process.env.LOG_FILTER = 'info,debug'
+
+    // Act
+    const config = Proxyquire('../../../src/lib/config', {})
+    
+    // Assert
+    assert.equal(config.customLevels, 'info,debug', 'Log levels match')
+    assert.end()
+  })
 
 
+  configTest.end()
+})
 
 
-//process.env.LOG_LEVEL overrides the default.json value
+//
 //process.env.LOG_FILTER overrides the default.json value
