f843bc626769cc1be59ab1f375660b40eccd854a
Shashikant Hirugade <shashi.pioneer@gmail.com>
2025-01-27 15:23:28 -0600

    fix: added missing type

---
+++ src/lib/constants.d.ts
@@ -0,0 +1,14 @@
+declare const allLevels: Readonly<{
+    error: 0;
+    warn: 1;
+    audit: 2;
+    trace: 3;
+    info: 4;
+    perf: 5;
+    verbose: 6;
+    debug: 7;
+    silly: 8;
+  }>;
+  
+  export { allLevels };
+  
