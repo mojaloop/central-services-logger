870663ab0487310d3cd87b6d2c65fce8f3ff6dad
geka.evk <geka.evk@gmail.com>
2026-03-06 11:11:16 +0000

    feat(iad-582): exclude 'messaging.kafka.message.key' from REDACTED

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,10 +1,19 @@
 const allLevels = Object.freeze({
-  error: 0, warn: 1, audit: 2, trace: 3, info: 4, perf: 5, verbose: 6, debug: 7, silly: 8
+  error: 0,
+  warn: 1,
+  audit: 2,
+  trace: 3,
+  info: 4,
+  perf: 5,
+  verbose: 6,
+  debug: 7,
+  silly: 8
 })
 
 const SENSITIVE_KEY_EXCLUSIONS = Object.freeze([
   'context',
-  'stack'
+  'stack',
+  'messaging.kafka.message.key'
 ])
 
 const SENSITIVE_SUBSTRINGS = Object.freeze([
